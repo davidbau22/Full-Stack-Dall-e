cd0a0e3c93844564b33a9ae9c1a6c37e7c191b59
DavidBautista <davbauti@cisco.com>
2023-11-30 15:28:04 -0500

    Changing frontend fetch to deployed port

--- Server/index.js
+++ Server/index.js
@@ -22,7 +22,9 @@ const startServer = async () => {
   try {
     connectDB(process.env.MONGODB_URL);
     app.listen(8080, () => {
-      console.log(":D server has started on port http://localhost:8080");
+      console.log(
+        ":D server has started on port http://https://dall-e-i8pi.onrender.com"
+      );
     });
   } catch (error) {
     console.log(error);
